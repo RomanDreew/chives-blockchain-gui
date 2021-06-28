a30b4ca38687dd8e8212f24900d38e716d144ca4
HiveProject2021 <jwang123@nyit.edu>
2021-06-28 08:34:26 -0700

    restart

--- src/util/blockRewards.ts
+++ src/util/blockRewards.ts
@@ -5,10 +5,7 @@ const BLOCKS_PER_YEAR = 1681920;
 
 export function calculatePoolReward(height: number): Big {
   if (height === 0) {
-    return MOJO_PER_CHIA.times(1.1).times(7 / 8);
-  }
-  if (height % 10 == 9) {
-    return MOJO_PER_CHIA.times(3.3);
+    return MOJO_PER_CHIA.times(0).times(7 / 8);
   }
   if (height < 3 * BLOCKS_PER_YEAR) {
     return MOJO_PER_CHIA.times(2*100).times(7 / 8);
@@ -22,16 +19,12 @@ export function calculatePoolReward(height: number): Big {
   if (height < 12 * BLOCKS_PER_YEAR) {
     return MOJO_PER_CHIA.times(0.25*100).times(7 / 8);
   }
-
   return MOJO_PER_CHIA.times(0.125*100).times(7 / 8);
 }
 
 export function calculateBaseFarmerReward(height: number): Big {
   if (height === 0) {
-    return MOJO_PER_CHIA.times(2.2).times(1 / 8);
-  }
-  if (height % 10 == 9) {
-    return MOJO_PER_CHIA.times(6.6);
+    return MOJO_PER_CHIA.times(0).times(1 / 8);
   }
   if (height < 3 * BLOCKS_PER_YEAR) {
     return MOJO_PER_CHIA.times(2*100).times(1 / 8);
